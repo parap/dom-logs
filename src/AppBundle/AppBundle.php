60abd044173fe5d500d5fd773a9b9bb5db5cc348
Alex <spalchinda@gmail.com>
2016-01-23 00:38:37 +0200

    Fix class to fit ContainerAware interface
    https://trello.com/c/v1SGjSGz

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -3,7 +3,14 @@
 namespace AppBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class AppBundle extends Bundle
 {
+    private $container;
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
 }
