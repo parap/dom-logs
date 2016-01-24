36f3ac3ade7057f908f0c12f16ba3f41cce39999
Alex <spalchinda@gmail.com>
2016-01-24 20:00:21 +0200

    Create template and use it in default controller
    This commit has goal to change base template to given one.
    
    https://trello.com/c/z1OQcTRN

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,16 +5,16 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @Template("AppBundle:default:base.html.twig")
      */
     public function indexAction(Request $request)
     {
-        return $this->render('default/base.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
+        return ['base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..')];
     }
 }
