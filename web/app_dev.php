906961d1f00020b26f500e2820532c76ce339d3a
Alex <spalchinda@gmail.com>
2016-01-27 21:06:42 +0200

    Enable override of Http method
    https://trello.com/c/fYVxaYS0

--- web/app_dev.php
+++ web/app_dev.php
@@ -26,6 +26,7 @@ Debug::enable();
 
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
+Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
