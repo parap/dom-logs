e24d8abc6bf318eac3e8f64436f9e2f038d292dc
Alex <spalchinda@gmail.com>
2016-01-27 17:37:49 +0200

    Add generated by CRUD templates, controller and form type
    https://trello.com/c/fYVxaYS0

---
+++ src/AppBundle/Controller/GameController.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Entity\Game;
+use AppBundle\Form\GameType;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Game controller.
+ *
+ * @Route("/game")
+ */
+class GameController extends Controller
+{
+    /**
+     * Lists all Game entities.
+     *
+     * @Route("/", name="game_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $games = $em->getRepository('AppBundle:Game')->findAll();
+
+        return $this->render('game/index.html.twig', array(
+            'games' => $games,
+        ));
+    }
+
+    /**
+     * Creates a new Game entity.
+     *
+     * @Route("/new", name="game_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $game = new Game();
+        $form = $this->createForm(get_class(new GameType()), $game);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($game);
+            $em->flush();
+
+            return $this->redirectToRoute('game_show', array('id' => $game->getId()));
+        }
+
+        return $this->render('game/new.html.twig', array(
+            'game' => $game,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Game entity.
+     *
+     * @Route("/{id}", name="game_show")
+     * @Method("GET")
+     */
+    public function showAction(Game $game)
+    {
+        $deleteForm = $this->createDeleteForm($game);
+
+        return $this->render('game/show.html.twig', array(
+            'game' => $game,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Game entity.
+     *
+     * @Route("/{id}/edit", name="game_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Game $game)
+    {
+        $deleteForm = $this->createDeleteForm($game);
+        $editForm = $this->createForm(new GameType(), $game);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($game);
+            $em->flush();
+
+            return $this->redirectToRoute('game_edit', array('id' => $game->getId()));
+        }
+
+        return $this->render('game/edit.html.twig', array(
+            'game' => $game,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a Game entity.
+     *
+     * @Route("/{id}", name="game_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Game $game)
+    {
+        $form = $this->createDeleteForm($game);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($game);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('game_index');
+    }
+
+    /**
+     * Creates a form to delete a Game entity.
+     *
+     * @param Game $game The Game entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Game $game)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('game_delete', array('id' => $game->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
---
+++ src/AppBundle/Form/GameType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class GameType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('pretender')
+            ->add('planGeneral')
+            ->add('map')
+            ->add('planResearch')
+            ->add('winner')
+            ->add('serverLink')
+            ->add('thread')
+            ->add('nation')
+            ->add('user')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Game'
+        ));
+    }
+}
