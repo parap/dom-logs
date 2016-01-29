61ba1af2d273e8e9a9bfc7d1272100d7bc1b3cd8
Alex <spalchinda@gmail.com>
2016-01-29 12:11:09 +0200

    Add maps table
    https://trello.com/c/KB06aGEh

---
+++ app/src/AppBundle/Migration/Version20160129100811.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\Migration;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160129100811 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $sql = "CREATE TABLE IF NOT EXISTS `maps` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=36 ;";
+
+        $this->addSql($sql);
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $this->addSql("DROP TABLE `maps`");
+    }
+}
