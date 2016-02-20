263344a0d734d91f63dcd45625e0ed181001746a
Alex <spalchinda@gmail.com>
2016-02-20 12:06:40 +0200

    Add methods to count in and out files
    https://trello.com/c/vAXBhsDF

--- src/AppBundle/Repo/TurnRepository.php
+++ src/AppBundle/Repo/TurnRepository.php
@@ -10,4 +10,17 @@ use Doctrine\ORM\Query\ResultSetMapping;
 
 class TurnRepository extends EntityRepository
 {
+    public function countInFiles()
+    {
+        $qb = $this->createQueryBuilder('t')->select('count(t.id) total')->where('t.turnInName IS NOT NULL');
+
+        return (int) $qb->getQuery()->getSingleScalarResult();
+    }
+
+    public function countOutFiles()
+    {
+        $qb = $this->createQueryBuilder('t')->select('count(t.id) total')->where('t.turnOutName IS NOT NULL');
+
+        return (int) $qb->getQuery()->getSingleScalarResult();
+    }
 }
