7705e3ed0b87afa9cf3c306f5cdd5ff831dd5b56
Alex <spalchinda@gmail.com>
2016-02-20 12:01:34 +0200

    Add method to count all going games (with no winner)
    https://trello.com/c/vAXBhsDF

--- src/AppBundle/Repo/GameRepository.php
+++ src/AppBundle/Repo/GameRepository.php
@@ -10,4 +10,10 @@ use Doctrine\ORM\Query\ResultSetMapping;
 
 class GameRepository extends EntityRepository
 {
+    public function countAllGoing()
+    {
+        $qb = $this->createQueryBuilder('g')->select('count(g.id) total')->where('g.winner IS NULL');
+
+        return (int) $qb->getQuery()->getSingleScalarResult();
+    }
 }
