866d788af8c596ef9b34f159514e94c5d8ab6de5
Alex <spalchinda@gmail.com>
2016-02-20 15:32:31 +0200

    Add comments and make class more descriptive
    https://trello.com/c/jF1uFOAB

--- html/js/scripts.js
+++ html/js/scripts.js
@@ -11,11 +11,13 @@ function disableOtherThan(age) {
 
 // delay after document is ready
 $(function () {
+//    Manage ages and nations on new game and edit game pages
     $('#game_age').on('change', function (e) {
         var age = $('#game_age>option:selected').text();
         disableOtherThan(age);
     });
 
+//    activate tooltips
     if ($.fn.tooltipster) {
         $('.tooltip').tooltipster({
             contentAsHTML: true,
@@ -23,8 +25,8 @@ $(function () {
         });
     }
 
-    if ($('#show-turn').length > 0) {
-
+//    have download links without hash
+    if ($('#activate-plain-download').length > 0) {
         $('a').each(function(e){
             var link = $(this);
             if ('download' == link.html()) {
