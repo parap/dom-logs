0b71409f8667a7af8dbc6730cbc58f9356afeb22
Alex <spalchinda@gmail.com>
2016-02-25 16:08:55 +0200

    Setup initial sample usage of autosuggesting script
    https://trello.com/c/hjW6yHKQ

--- html/js/scripts.js
+++ html/js/scripts.js
@@ -35,6 +35,20 @@ $(function () {
             }
         });
     }
+
+    if ($('#titles').length > 0) {
+        $('#title').autoComplete({
+            minChars: 2,
+            source: function(term, suggest){
+                term = term.toLowerCase();
+                var choices = ['ActionScript', 'AppleScript', 'Asp'];
+                var matches = [];
+                for (i=0; i<choices.length; i++)
+                    if (~choices[i].toLowerCase().indexOf(term)) matches.push(choices[i]);
+                suggest(matches);
+            }
+        });
+    }
 });
 
 
